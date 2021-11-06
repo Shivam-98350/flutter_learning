10cb41f4775f920564bc2edb6a3609c23685efa6
Shivam Kumar <shivamkumarbth19@gmail.com>
2021-11-07 00:04:51 +0530

    day 9 completd

---
+++ lib/models/catalog.dart
@@ -0,0 +1,10 @@
+class item {
+  final String name;
+  final String id;
+  final String colour;
+  final String desp;
+  final num price;
+  final String image;
+
+  item(this.name, this.id, this.colour, this.desp, this.price, this.image);
+}
